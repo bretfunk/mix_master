7f231268f45c06f8d09a61c2cea9639ac3133b53
BFunk <bret.e.funk@gmail.com>
2017-07-12 17:50:06 -0600

    added a shared error message

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,4 +1,6 @@
 class PlaylistsController < ApplicationController
+  before_action :set_playlist, only: [:show, :edit, :update]
+
   def index
     @playlists = Playlist.all
   end
@@ -15,16 +17,13 @@ class PlaylistsController < ApplicationController
   end
 
   def show
-    @playlist = Playlist.find(params[:id])
   end
 
   def edit
-    @playlist = Playlist.find(params[:id])
     @songs = Song.all
   end
 
   def update
-    @playlist = Playlist.find(params[:id])
     @playlist.update(playlist_params)
 
     redirect_to @playlist
@@ -34,5 +33,9 @@ class PlaylistsController < ApplicationController
 
   def playlist_params
     params.require(:playlist).permit(:name, song_ids: [])
+  end
+
+  def set_playlist
+    @playlist = Playlist.find(params[:id])
   end
 end
